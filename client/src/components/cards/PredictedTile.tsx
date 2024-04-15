36c3f0b6a7dae3292664b2918352292835b4620a
Mitchell De Nardis <mitchelldenardis@gmail.com>
2024-04-15 17:35:52 +1000

    Added predicted birds radio group with confidence chips

--- client/src/components/cards/PredictedTile.tsx
+++ client/src/components/cards/PredictedTile.tsx
@@ -1,4 +1,5 @@
-import { Bird, defaultBirds } from '../../lib/birds'
+import { Bird, defaultBirds, confidenceChipStyles } from '../../lib/birds'
+import { RadioGroup, Radio, Chip } from '@nextui-org/react'
 
 type Props = {
   birds?: Bird[]
@@ -6,13 +7,31 @@ type Props = {
 
 export const PredictedTile = ({ birds = defaultBirds }: Props) => {
   return (
-    <>
-      {birds.map((b: Bird, i: any) => (
-        <div className="mb-2" key={i}>
-          <div>Name: {b.name}</div>
-          <div>Confidence: {b.confidence}</div>
-        </div>
-      ))}
-    </>
+    // TODO add a button for "looks similar"
+    <div className="overflow-x-hidden pr-2">
+      {birds && (
+        <RadioGroup className="flex flex-wrap" orientation="horizontal">
+          {birds.map((b: Bird, i: any) => (
+            <Radio
+              key={i}
+              value={i}
+              size="lg"
+              color="success"
+              className={'my-0 ml-2 rounded-xl border border-black'}
+            >
+              <div className="flex grow flex-row">
+                <div>{b.name}</div>
+                <Chip
+                  size="lg"
+                  className={`ml-2 ${confidenceChipStyles(b.confidence)}`}
+                >
+                  {b.confidence}
+                </Chip>
+              </div>
+            </Radio>
+          ))}
+        </RadioGroup>
+      )}
+    </div>
   )
 }
